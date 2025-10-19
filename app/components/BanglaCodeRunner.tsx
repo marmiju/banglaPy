82c920f88006e8141312934ee921c68625ba0c1b
MAR miju <developerazizar@gmail.com>
2025-10-19 17:26:13 +0600

    .env

--- app/components/BanglaCodeRunner.tsx
+++ app/components/BanglaCodeRunner.tsx
@@ -16,7 +16,7 @@ export default function BanglaCodeRunner() {
     setPythonCode("");
 
     try {
-      const res = await fetch("http://localhost:5000/toPython", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/toPython`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ code: banglaCode }),
