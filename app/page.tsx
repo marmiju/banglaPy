2de4f956df14b3ded5acb03695b6d389a9a16689
MAR miju <developerazizar@gmail.com>
2025-10-20 06:55:31 +0600

    if-elif-else

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,15 @@ export default function Home() {
          <p className="max-w-[600px]">আমরা তৈরি করেছি একটি বাংলা-প্যথন ল্যাঙ্গুয়েজ কনভার্টার, যা শিশুরা সহজে বাংলাতে ইনপুট দিয়ে প্যথনে কোড চালাতে পারবে। এই টুলটি প্রোগ্রামিং শেখার প্রক্রিয়াকে আরো সহজ, মজা এবং সাশ্রয়ী করে তোলে, যেখানে শিশুরা তাদের মাতৃভাষায় কোডিং শিখতে এবং সমস্যা সমাধান করতে পারে। আজই প্রোগ্রামিংয়ের জগতে তাদের প্রথম পদক্ষেপ নেওয়ার সময়!</p>
          <div className="space-x-4">
           <Link href={'/banglapy'} className="bg-purple-600 text-white px-4 py-2"> প্রাকটিস করুন</Link>
-          <Link href={'/banglapy'} className="bg-slate-200 text-purple-600 px-4 py-2">শিখুন</Link>
+          <Link
+  href="https://github.com/marmiju/banglaPy" 
+  target="_blank" 
+  rel="noopener noreferrer"
+  className="bg-slate-200 text-purple-600 px-4 py-2"
+>
+  শিখুন
+</Link>
+
          </div>
       </div>
     </main>
