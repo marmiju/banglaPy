1ad6031f38ebc191a078840fbad8a0d084fd95b6
MAR_mju <developerazizar@gmail.com>
2025-10-26 00:18:47 +0600

    add markdown

--- app/components/lwv/LWV.tsx
+++ app/components/lwv/LWV.tsx
@@ -1,19 +1,37 @@
-import React from 'react'
+'use client';
+import React, { Suspense } from 'react'
 import BanglaCodeRunner from '../BanglaCodeRunner'
+
+import ReactMarkdown from 'react-markdown';
+
 interface props {
-    topic:string,
-    desc:string,
-    source?: string,
+    explaination: string
+    source?: string
     code?: string
 }
 
-const LWV = ({ source, code,desc,topic }: props) => {
+const LWV = ({ source, code, explaination }: props) => {
     return (
-        <div className=''>
-            <h3 className='text-xl text-red-400'>{topic}</h3>
-            <p className=''>{desc}</p>
-            <div className='flex flex-col md:flex-row'>
-               {source &&  <iframe className='w-full object-contain rounded h-80 md:h-96 md:w-1/2' src={`https://www.youtube.com/embed/${source}`} title="Moner majhe acho tomi।  Islamic song। kalarab song।  2017।" allow="accelerometer;  clipboard-write; encrypted-media; gyroscope;" allowFullScreen></iframe>}
+        <div>
+
+            <div  className="markdown-body">
+                <ReactMarkdown>{explaination}</ReactMarkdown>
+            </div>
+
+
+
+
+            <div className='flex flex-col md:flex-row mt-4'>
+                <Suspense fallback={<div className='w-64 h-6d'>Waiting for Video</div>}>
+                    {source && (
+                        <iframe
+                            className='w-full object-contain rounded h-80 md:h-96 md:w-1/2'
+                            src={`https://www.youtube.com/embed/${source}`}
+                            allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
+                            allowFullScreen
+                        ></iframe>
+                    )}
+                </Suspense>
                 {code && <BanglaCodeRunner input={code} />}
             </div>
         </div>
