61d80279d79f51a87401591bb2764d3456ac8ab1
MAR_mju <developerazizar@gmail.com>
2025-11-12 16:50:45 +0600

    problem submission

--- app/components/BanglaCodeRunner.tsx
+++ app/components/BanglaCodeRunner.tsx
@@ -2,14 +2,18 @@
 import { useState } from "react";
 import Editor from "@monaco-editor/react";
 import { saveLocalStorege } from "@/utils/SaveCodeInLicalStore";
+import { useUserContext } from "./hooks/provider/ContextApi";
+import { Problem } from "@/utils/types/userInterface";
+import { toast, ToastContainer } from "react-toastify";
 
 interface prpos {
   src_code?: string
-  std_input?: string
+  problem?: Problem
   height?: string
+  problemId?: string
 }
 
-export default function BanglaCodeRunner({ src_code, std_input, height }: prpos) {
+export default function BanglaCodeRunner({ src_code, problem, height }: prpos) {
   const [banglaCode, setBanglaCode] = useState(src_code);
   const [pythonCode, setPythonCode] = useState("");
   const [output, setOutput] = useState("");
@@ -17,7 +21,54 @@ export default function BanglaCodeRunner({ src_code, std_input, height }: prpos)
   const [error, setError] = useState("");
   const [stderr, setstderr] = useState("");
 
+  const { user } = useUserContext()
+
   const [copied, setCopied] = useState(false)
+
+  const submitcode = async () => {
+    setLoading(true);
+    setError("");
+    setstderr("");
+    setOutput("");
+    setPythonCode("");
+
+
+    try {
+      if (!user) return setError("প্রথমে লগইন করুন")
+
+      if (!banglaCode) return
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/submitcode`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ code: banglaCode, input: problem?.sampleInput, userId: user.id, problemId: problem?.id }),
+      });
+
+      
+
+      const data = await res.json();
+      if (data.isCorrect) {
+        toast.success(data.msg, );
+      }else{
+        toast.error(data.msg);
+      }
+
+      if (!res.ok) throw new Error(data.error || "Server error");
+      saveLocalStorege(banglaCode!);
+      setPythonCode(data.pythonCode || "");
+      if (data.stderr) {
+        setstderr(data.stderr);
+        console.log("Stderr:", data.stderr);
+      } else {
+        setOutput(data.output || "No output");
+      }
+
+    } catch (err) {
+      console.error("err", err);
+      setError("মনে হচ্ছে কিছু সমস্যা আছে");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const runCode = async () => {
     setLoading(true);
@@ -33,10 +84,10 @@ export default function BanglaCodeRunner({ src_code, std_input, height }: prpos)
       if (!banglaCode) return
 
 
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/toPython`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/runcode`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ code: banglaCode, input: std_input }),
+        body: JSON.stringify({ code: banglaCode, input: problem?.sampleInput }),
       });
 
       const data = await res.json();
@@ -71,13 +122,14 @@ export default function BanglaCodeRunner({ src_code, std_input, height }: prpos)
 
   return (
     <div className=" p-0  mb-20 text-black/80  flex flex-col lg:flex-row gap-4  w-full max-w-[1280px] mx-auto">
+      <ToastContainer />
       {/* ---- Code Editor ---- */}
       <div className=" min-w-1/2 w-full flex flex-col rounded-lg overflow-hidden">
 
         <pre className="text-white bg-slate-600 "> .বাংলা</pre>
         <Editor
           className="border border-slate-400"
-          height={height|| '300px'}
+          height={height || '300px'}
           width={"400"}
           defaultLanguage="python"
           language="python"
@@ -94,7 +146,11 @@ export default function BanglaCodeRunner({ src_code, std_input, height }: prpos)
         {error && <p className="text-red-500 text-center bg-slate-300 z-10 mt-[-10px]">{error}</p>}
         {
           loading ? <div className="text-center text-black bg-white/80  p-2">আপেক্ষা করুন</div>
-            : <button disabled={loading} onClick={runCode} className={`bg-gradient-to-br bg-white text-black px-4 cursor-pointer py-2 `}>কোড রান করুন</button>
+            : <div className="w-full flex flex-col md:flex-row ">
+              <button disabled={loading} onClick={runCode} className={`bg-gradient-to-br w-full bg-white text-black px-4 cursor-pointer py-2 `}>কোড রান করুন</button>
+              {window.location.pathname.startsWith('/problems') && <button disabled={loading} onClick={submitcode} className={`bg-gradient-to-br w-full bg-green-600 text-white px-4 cursor-pointer py-2 `}>সাবমিট করুন</button>}
+
+            </div>
         }
       </div>
 
--- app/problems/[id]/page.tsx
+++ app/problems/[id]/page.tsx
@@ -1,8 +1,6 @@
 import BanglaCodeRunner from "@/app/components/BanglaCodeRunner";
 import ProblemSec from "@/app/components/problemSec/ProblemSec";
-import { Problem_res } from "@/public/data/problems";
 import { getProblem } from "@/utils/functions/GetProblem";
-import { Suspense } from "react";
 
 interface Props {
     params: { id: string }
@@ -15,8 +13,9 @@ const Page = async ({ params }: Props) => {
 
     return (
         <div className="max-w-[1280px] mx-auto">
+            
             <ProblemSec problem={problem} />
-            <BanglaCodeRunner std_input={problem.sampleInput} />
+            <BanglaCodeRunner problem={problem} />
         </div>
     )
 }
